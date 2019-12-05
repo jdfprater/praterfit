230a46e0633b5c086356d3e7f7e9985bfa66dc12
John <jdfprater@gmail.com>
2019-12-04 20:34:32 -0600

    Update heart.js

--- js/heart.js
+++ js/heart.js
@@ -11,8 +11,8 @@ var maxHR = 0,
 function heartZones() {
     'use strict';
     var x = document.getElementById("heartForm"),
-        age = x.elements.Age.value,
-        rhr = x.elements.RHR.value;
+        age = Number(x.elements.Age.value),
+        rhr = Number(x.elements.RHR.value);
     
     maxHR = Math.round(207 - (0.7 * age));
     HRR = maxHR - rhr;
@@ -23,10 +23,10 @@ function heartZones() {
     z4 = Math.round((HRR * 0.8) + rhr);
     z5 = Math.round((HRR * 0.9) + rhr);
      
-     document.getElementById("zone1").innerHTML = z1 + " BPM";
-     document.getElementById("zone2").innerHTML = z2 + " BPM";
-     document.getElementById("zone3").innerHTML = z3 + " BPM";
-     document.getElementById("zone4").innerHTML = z4 + " BPM";
-     document.getElementById("zone5").innerHTML = z5 + " BPM";
+    document.getElementById("zone1").innerHTML = z1 + " BPM";
+    document.getElementById("zone2").innerHTML = z2 + " BPM";
+    document.getElementById("zone3").innerHTML = z3 + " BPM";
+    document.getElementById("zone4").innerHTML = z4 + " BPM";
+    document.getElementById("zone5").innerHTML = z5 + " BPM";
     
 }
